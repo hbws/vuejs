91756ff2678c34a9ddf712d951c4e51479cafbec
hbw <3185309920@qq.com>
2019-08-12 15:01:53 +0800

    本地工程添加到git

---
+++ git-test/src/com/cssl/test/test.java
@@ -0,0 +1,12 @@
+package com.cssl.test;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("git test");
+        System.out.println("git test");
+        System.out.println("git test");
+        System.out.println("git test");
+        System.out.println("git test");
+        System.out.println("git test");
+    }
+}
