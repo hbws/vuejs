488d517ff063d50e0f69982a84e03f04f9727fb9
hbw <3185309920@qq.com>
2019-08-23 15:09:36 +0800

    向本地仓库添加一个vue学习项目

---
+++ vuetest/src/test/java/com/css/vue/AppTest.java
@@ -0,0 +1,20 @@
+package com.css.vue;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
